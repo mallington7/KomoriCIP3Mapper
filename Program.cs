38504009b82d2134da651759ad61a2f6daa20fe4
Mark <mallington7@gmail.com>
2020-07-29 10:33:48 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//Entry point
+namespace KomoriCIP3Mapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Run(args[0],Convert.ToInt32 (args[1]));
+        }
+
+        static void Run(string scanDir,int daysToProcess)
+        {
+            var komirServ = new Services.KomoriUtil();
+            string[] files = Directory.GetFiles(scanDir, "*.PQ4", SearchOption.AllDirectories);
+
+            foreach (string PQ4File in files)
+            {
+              var fileCreated=  File.GetCreationTime(PQ4File);
+
+                if (fileCreated> DateTime .Now.AddDays (-daysToProcess))
+                {
+                    var resultingFlat = komirServ.FixFlatName(PQ4File);
+                    Console.WriteLine(resultingFlat);
+                }
+              
+            }
+        }
+
+    }
+}
---
+++ Services/KomoriUtil.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KomoriCIP3Mapper.Services
+{
+    class KomoriUtil
+    {
+
+        public string FixFlatName(string fileName)
+        {
+            //186140 Bosch Thermotechnology Ltd!186140-30714_Bosch Thermotechnology Ltd_8 A .pdf[b].1A.CMYK
+            int idx = fileName.LastIndexOf('_');
+            string flatName ="";
+            string jobNo = "";
+            try
+            {
+                flatName=fileName.Substring(idx+1).Split('.')[0].Replace (" ","");
+                jobNo = Path.GetFileName(fileName).Substring(0, 6);
+            }
+            catch (Exception)
+            {
+              
+            
+            }
+
+          if (flatName !="" && idx>1)
+            {
+
+                //First make a copy
+           File.Copy(fileName, $"c:/orig/{Path.GetFileName(fileName)}",true);
+            string[] arrLine = File.ReadAllLines(fileName);
+            arrLine[4] = $"Job={jobNo}-{flatName}" ;
+            File.WriteAllLines(fileName, arrLine);
+            }
+
+            return $"Fixed File: {Path.GetFileName (fileName)} with flat code {flatName}";
+        }
+
+    } 
+}
